c8284a0b12facecf044eff8e06e7ab1a3cade675
Thymen <t.d.j.rijpkema@student.tue.nl>
2019-07-03 12:41:02 +0200

    setup of all files

--- pycman/stat/logger.py
+++ pycman/stat/logger.py
@@ -2,15 +2,18 @@
 
 
 def log_game(*args, **kwargs):
-    """"Logs all the given information at the end of a game. Will produce exactly one line per game. """
+    """ Logs all the given information at the end of a game. Will produce exactly one line per game. """
     pass
 
 
 def log_step(*args, **kwargs):
-    """"Logs all the given information for a particular step. Will produce exactly one line per step. """
+    """ Logs all the given information for a particular step. Will produce exactly one line per step. """
     pass
 
 
 def log(*args, **kwargs):
-    """"General logging function that can always log information at any given moment. Simply appends the log file. """
+    """ General logging function that can always log information at any given moment. Simply appends the log file. """
     pass
+
+
+def log_console(*args, **kwargs): ...
---
+++ setup.py
@@ -0,0 +1,5 @@
+
+
+
+import gym
+gym.make("MsPacman-v0")

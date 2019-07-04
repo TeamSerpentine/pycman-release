42992a9637c54b848b4bdb9a52b126ecbc0583fe
clepelaars <info@carlolepelaars.nl>
2019-07-04 19:14:19 +0200

    Loss Visualization and General Statistics skeleton

--- pycman/stat/statistics/general_statistics.py
+++ pycman/stat/statistics/general_statistics.py
@@ -1,13 +1,22 @@
-import numpy as np
 import pandas as pd
 
 
 class GeneralStats:
-    def __init__(self):
-        self.df = pd.read_csv("logging_file", delimiter="|")
+    def __init__(self, file_path):
+        self._file_path = file_path
+        self.df = pd.read_csv(self._file_path, delimiter="|")
 
     def summary_stats(self):
+        """
+        Returns summary statistics over evaluated games
+        - Mean and standard deviation of reward
+        - Average steps
+        -
+        """
         pass
 
     def action_distribution(self):
+        """
+        Returns a bar chart of the action distribution
+        """
         pass
--- pycman/stat/statistics/reward_visualization.py
+++ pycman/stat/statistics/reward_visualization.py
@@ -5,61 +5,45 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import tkinter as tk
 from tkinter import Checkbutton, Button, Tk
 
 
+# TODO Fix deleting of plots
 class RewardViz:
-    def __init__(self, file_name):
-        self._filename = file_name
-        # self.df = pd.read_csv("Logging File", delimiter="|")
+    def __init__(self, file_name, figsize=(10,5)):
+        self._file_name = file_name
+        # self.df = pd.read_csv(self._file_name, delimiter="|")
         self.baselines = pd.read_csv('deep_rl_scores.csv')
         self.window = Tk()
-        self.fig = Figure(figsize=(10, 5))
+        self.fig = Figure(figsize=figsize)
         self.ax = self.fig.add_subplot(111)
 
+        # Initialize main visualization buttons
         self.button = Button(self.window, text="Reward Visualization".center(30),
                              command=self.plot_reward)
         self.button.grid(row=0, column=0)
 
-        self.human_button = Checkbutton(self.window, text="Human Baseline".center(30),
-                                        command=lambda: self.plot_baseline("Human"))
-        self.human_button.grid(row=0, column=1)
+        # Initialize baseline check buttons
+        for num, name in enumerate(self.baselines.columns[1:]):
+            setattr(self, name, Checkbutton(self.window, text=f"{name} Baseline".center(30),
+                                            command=self.lambda_func(name)))
+            getattr(self, name).grid(row=num, column=1)
 
-        self.random_button = Checkbutton(self.window, text="Random Baseline".center(30),
-                                         command=lambda: self.plot_baseline("Random"))
-        self.random_button.grid(row=1, column=1)
-
-        self.dqn_button = Checkbutton(self.window, text="DQN Baseline".center(30),
-                                      command=lambda: self.plot_baseline("DQN"))
-        self.dqn_button.grid(row=2, column=1)
-
-        self.ddqn_button = Checkbutton(self.window, text="DDQN Baseline".center(30),
-                                       command=lambda: self.plot_baseline("DDQN"))
-        self.ddqn_button.grid(row=3, column=1)
-
-        self.duelingdqn_button = Checkbutton(self.window, text="Dueling DQN Baseline".center(30),
-                                             command=lambda: self.plot_baseline("Dueling DQN"))
-        self.duelingdqn_button.grid(row=4, column=1)
-
-        self.a3cff_button = Checkbutton(self.window, text="A3C Feed Forward Baseline".center(30),
-                                        command=lambda: self.plot_baseline("A3C FF"))
-        self.a3cff_button.grid(row=5, column=1)
-
-        self.a3clstm_button = Checkbutton(self.window, text="A3C LSTM Baseline".center(30),
-                                          command=lambda: self.plot_baseline("A3C LSTM"))
-        self.a3clstm_button.grid(row=6, column=1)
-
+        # Auxiliary buttons
         self.save_button = Button(self.window, text="Save Figure".center(30),
                                   command=self.save_figure)
-        self.save_button.grid(row=8, column=0)
+        self.save_button.grid(row=9, column=0)
 
+        # Initialize Figure
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
-        self.canvas.get_tk_widget().grid(row=7, column=0)
+        self.canvas.get_tk_widget().grid(row=8, column=0)
+
+    def lambda_func(self, name):
+        return lambda: self.plot_baseline(name)
 
     def plot_reward(self):
         # Placeholder plot
-        self.ax.plot(range(5), [1, 5, 8, 4, 23], color='blue', label=f"Reward")
+        self.ax.plot(range(5), [1, 5, 8, 4, 23], color='blue', label="Reward")
         # Format plot
         self.ax.set_title("Reward Over Games", fontsize=16)
         self.ax.set_xlabel('Nr. Games')
@@ -76,8 +60,11 @@ class RewardViz:
         self.canvas.draw()
         return self
 
-    def save_figure(self):
-        self.fig.savefig(f'plot_of_{self._filename}_reward', dpi=300)
+    def save_figure(self, dpi=300):
+        """
+        Saves the figure currently on display
+        """
+        self.fig.savefig(f'plot_of_{self._file_name}_reward', dpi=dpi)
         return self
 
     def display(self):
@@ -88,5 +75,12 @@ class RewardViz:
 if __name__ == '__main__':
     logging_file = "Logging_File_Name"
     reward = RewardViz(logging_file)
-    reward.display()
+
+    # Configure plot manually
+    reward.plot_reward()
+    reward.plot_baseline("DDQN")
+    reward.save_figure()
+
+    # Configure with GUI
+    #reward.display()
 

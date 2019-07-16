9c535c861bf73670f585c5996f78233355dc2318
Mickey Beurskens <mickeybeurskens@gmail.com>
2019-07-16 20:42:50 +0200

    Removed old commented code and print statements. Added fixme.

--- pycman/core/run/run.py
+++ pycman/core/run/run.py
@@ -30,39 +30,17 @@ class Run:
         args = []
         for agent, env in zip(self.agents, envs):
             args.append((agent, env))
-            print(str(agent) + "##############")
+            # FIXME: create test case -> print(str(agent) + "##############")
 
         with Pool(len(self.agents)) as p:
             result = p.map(self.start_worker, args)
 
         print(self.agents)
         for idx, a in enumerate(result):
-            #i, j = self.agents.get_weird_indices(idx)
             self.agents[idx] = a
-            #self.agents.nested_store[i][j] = a
         return
 
     @staticmethod
     def start_worker(info):
         info[0].run(info[1])
         return info[0]
-
-
-    # def run_parallel(self):
-    #     envs = [deepcopy(self.env.get()) for _ in self.agents]
-    #
-    #     processes = []
-    #     for agent, env in zip(self.agents, envs):
-    #         p = Process(target=agent.run, args=(env,))
-    #         processes.append(p)
-    #
-    #     # Start the processes
-    #     for p in processes:
-    #         print(f"{str(agent).ljust(20)}, {env}")
-    #         p.start()
-    #
-    #     # Ensure all processes have finished execution
-    #     for p in processes:
-    #         p.join()
-    #
-    #    return

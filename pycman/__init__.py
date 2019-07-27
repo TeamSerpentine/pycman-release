8a1e44491c5e713be3dd736e89cdde31bd98e1f5
Wouter <w.j.g.m.v.d.bemd@student.tue.nl>
2019-07-27 15:14:29 +0200

    Line logging now works properly for single agent or sequential processing of multiple agents.

--- pycman/__init__.py
+++ pycman/__init__.py
@@ -2,7 +2,7 @@ from pycman.core.utils.decorators import _timer
 from pycman.core.session.session import _Session, _AgentSet, _SelectedEnv
 from pycman.core.env.env_base import EnvBase
 from pycman.core.agent.agent_base import AgentBase
-
+from pycman.core.logger.simple_logger import Log
 
 __all__ = ["env", "agent", "run", "EnvBase", "AgentBase"]
 
@@ -13,3 +13,4 @@ __session = _Session(agent, env)
 
 # Function facade
 run = __session.run
+log = Log("Hello")
--- pycman/examples/advanced_agent.py
+++ pycman/examples/advanced_agent.py
@@ -1,39 +1,37 @@
 import pycman
 
 
-class RandomAgent(pycman.AgentBase):
-    def __init__(self):
-        self.output_shape = ...
-        self.input_shape = ...
-
-        self.loss = None
-        self.reward = None
+class AdvancedAgent(pycman.AgentBase):
+    def __init__(self, number):
+        self.number = number
         self.games_played = 0
 
     def run(self, env):
         for _ in range(2):
             env.reset()
             done = False
+            rewards = []
             while not done:
                 # env.render()
                 obs, reward, done, info = env.step(env.random_action())
+                rewards.append(reward)
             self.games_played += 1
+            pycman.log.line(self.number, self.games_played, sum(rewards))
 
 
 if __name__ == "__main__":
 
+    #pycman.log.line("First Line!")
     pycman.env.gym("Breakout-v0")
 
-    agent = [RandomAgent()]
-    pycman.agent.add(agent)
-    pycman.run(order='parallel')
-
-    pycman.env.gym("Breakout-v0")
-    pycman.agent.clear()
+    # agent = [AdvancedAgent(0)]
+    # pycman.agent.add(agent)
+    # pycman.run(order='parallel')
+    # pycman.agent.clear()
 
     # Test with many
-    agents4 = [RandomAgent() for _ in range(4)]
-    agents2 = [RandomAgent() for _ in range(2)]
+    agents4 = [AdvancedAgent(i+1) for i in range(4)]
+    agents2 = [AdvancedAgent(i+5) for i in range(2)]
 
     print(len(pycman.agent))
     pycman.agent.add(agents4)
@@ -41,7 +39,7 @@ if __name__ == "__main__":
     pycman.agent.add(agents2)
     print(len(pycman.agent))
     pycman.run(order='parallel')
-    pycman.run(order='sequential')
+    #pycman.run(order='sequential')
 
     # TODO: Raise error if no agents are present.
     # TODO: RAISE error if environment is not set.

252fa61751cbbfc43f7716c87099722bee956329
Wouter <w.j.g.m.v.d.bemd@student.tue.nl>
2019-07-27 11:02:15 +0200

    Minor refactor, removed static method from Agent base class.

--- pycman/__init__.py
+++ pycman/__init__.py
@@ -3,11 +3,13 @@ from pycman.core.session.session import Session, AgentSet, SelectedEnv
 from pycman.core.env.env_base import Env
 
 
-__all__ = ["env", "agent", "session", "timer"]
+__all__ = ["env", "agent"]
 
-
+# Creating global classes
 env = SelectedEnv()
 agent = AgentSet()
-session = Session(agent, env)
-run = session.run
+__session = Session(agent, env)
 
+# Function facade
+run = __session.run
+clear = __session.clear
--- pycman/core/agent/agent.py
+++ pycman/core/agent/agent.py
@@ -6,11 +6,8 @@ from abc import ABC, abstractmethod
 
 
 class Agent(ABC):
+
     @abstractmethod
     def run(self, env, max_thread=1):
         """ Run the agent on the given environment until completion. """
         pass
-
-    @staticmethod
-    def get_environment_info(env: pycman.env):
-        return env.get_constants()
